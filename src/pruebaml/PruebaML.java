4c85bc42471b03cb210b58ced8e2ffb17a1c867d
Facundo b <facundobustamante21@gmail.com>
2024-10-18 20:39:25 -0300

    Subida inicial

---
+++ src/pruebaml/PruebaML.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebaml;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 46390835
+ */
+public class PruebaML {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here>
+        
+        ArrayList<Integer> ci = new ArrayList <Integer>();
+        
+        ci.add (46390835);
+        
+        System.out.println("Cedula ingresada " + ci.get(0));
+    }
+    
+}
